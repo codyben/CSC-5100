4bca9d5107cc5c94eb163e5d9feaa0e997c65b39
Cody Benkoski <cob322@lehigh.edu>
2022-03-06 17:09:50 -0500

    Better example!

--- NormalCar.py
+++ NormalCar.py
@@ -1,14 +1,29 @@
 from agents.navigation.behavior_agent import BehaviorAgent
 class NormalCar:
-    def __init__(self, model, speed, lane_change=False, behavior=BehaviorAgent):
-        pass
+    def __init__(self, blueprint, world, spawn, speed = 80, lane_change=False, behavior=BehaviorAgent):
+        self.blueprint = blueprint
+        self.world = world
+        self.speed = speed
+        self.lane_change = lane_change
+        self.behavior = behavior
+        self.spawn = spawn
+        self.vehicle = self.world.spawn_actor(self.blueprint, spawn)
+        self.agent = behavior(self.vehicle)
+        self.agent.set_target_speed(self.speed)
+        self.waypoint_set = False
 
-    def carla_setup(world, traffic_mgr):
-        pass
-    def spawn_point(spawn_point):
-        pass
-    def set_waypoint(waypoint):
-        pass
-    def force_lane_change():
+    def goto(self, waypoint):
+        self.agent.set_destination(waypoint.transform.location)
+        self.waypoint_set = True
+
+    def done(self):
+        return self.agent.done()
+
+    def drive(self, callback=lambda : False):
+        if self.agent and self.waypoint_set:
+            while not self.done():
+                self.vehicle.apply_control(self.agent.run_step())
+        callback()
+    def force_lane_change(self):
         pass
     
---
+++ example.py
@@ -0,0 +1,28 @@
+
+import carla
+from agents.navigation.controller import VehiclePIDController
+import random
+# To import a behavior agent
+from agents.navigation.behavior_agent import BehaviorAgent
+from NormalCar import NormalCar as Car
+
+
+client = carla.Client('localhost', 2000)
+client.set_timeout(2.0)
+world = client.get_world()
+world_map = world.get_map()
+waypoint02 = world_map.get_waypoint_xodr(60,-6,0) #Used this to get coords: https://odrviewer.io/
+spawn_points = world.get_map().get_spawn_points()
+vehicle_blueprint = world.get_blueprint_library().filter('model3')[0]
+
+
+s = random.choice(spawn_points)
+
+b_currier = lambda b: BehaviorAgent(b, behavior='aggressive')
+
+single_car = Car(vehicle_blueprint, world, s, behavior=b_currier)
+single_car.goto(waypoint02)
+single_car.drive(lambda : print("Done driving!"))
+
+
+  
